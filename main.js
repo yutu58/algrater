31491b901cdc65fe42e1f59d8716f02f5de5727e
yutu58 <165717@driestarleerling.nl>
2019-08-05 23:34:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,37 +2,35 @@
 var Rthumb = 0
 var Lthumb = 0
 var Points;
+var turns;
 var tbody = document.getElementById("tbody"), row, cell1, cell2
 
-//needed functions from own library
+//needed functions from library
 function sortTable(tbody, index, ascending) {
     Array.prototype.slice.call(tbody.children).sort(
-        (tr1, tr2) => tr1.children[index].textContent.localeCompare(tr2.children[index].textContent) * (ascending ? 1 : -1)
+        (tr1, tr2) => tr1.children[index].textContent.localeCompare(tr2.children[index].textContent, undefined, {'numeric': true}) * (ascending ? 1 : -1)
         ).forEach(tr => tbody.appendChild(tr));
 }
 
 //actual script
 function printvalue() {
 document.getElementById("tbody").innerHTML = ""
-var alg = document.getElementById("algtest").value.split("\n")
-for (var i=0; i<alg.length; i=i+1){
+var algs = document.getElementById("algtest").value.split("\n")
+algs.forEach(function(p) {
 	Points = 0
-    Rthumb = 0
-    Lthumb = 0
-	var turns = alg[i].split(" ")
-	for (var j=0; j<turns.length; j=j+1){
-		move(turns[j])
-	}
-    	//add to the table
+	Rthumb = 0
+	Lthumb = 0
+	p.split(" ").forEach(move)
+	//add to the table
 	if (Points != 0) {
-    row = tbody.insertRow()
-    cell1 = row.insertCell()
-    cell1.innerHTML = alg[i]
-    cell2 = row.insertCell()
-    cell2.innerHTML = Points
+    	row = tbody.insertRow()
+    	cell1 = row.insertCell()
+    	cell1.innerHTML = p
+    	cell2 = row.insertCell()
+    	cell2.innerHTML = Points
 	}
-}
-sortTable(document.querySelector("tbody"), 1, true);
+});
+sortTable(document.getElementById("tbody"), 1, true);
 }
 
 function move(turn) {
@@ -40,33 +38,41 @@ if (Rthumb == 0 && Lthumb == 0) {
 	switch(turn) {
     case "R":
     	Rthumb = 1
-        Points += 1
+        Points += 5
 	break;
     case "R'":
     	Rthumb = 3
-        Points += 1
+        Points += 5
 	break;
 	case "R2":
     	Rthumb = 2
-		Points += 1.3
+		Points += 9
 	break;
     case "U":
-    	Points += 1
+    	Points += 5
 	break;
     case "U'":
-    	Points += 1
+    	Points += 5
 	break;
 	case "U2":
-    	Points += 1.3
+    	Points += 8
 	break;
     case "F":
-    	Points += 1.2
+    	Points += 11
 	break;
     case "F'":
-    	Points += 1.2
+    	Points += 9
 	break;
     case "F2":
-    	Points += 1.7
+    	Points += 19
+	break;
+    case "D":
+    	Points += 5
+	break;
+    case "D'":
+    	Points += 5
+	case "D2":
+    	Points += 8
 	break;
 	}
 }
@@ -74,33 +80,42 @@ else if (Rthumb == 1 && Lthumb == 0) {
 	switch(turn) {
     case "R":
     	Rthumb = 2
-        Points += 1.2
+        Points += 9
 	break;
     case "R'":
     	Rthumb = 0
-        Points += 1
+        Points += 5
 	break;
 	case "R2":
     	Rthumb = 3
-		Points += 1.2
+		Points += 9
 	break;
     case "U":
-    	Points += 1.2
+    	Points += 9
 	break;
     case "U'":
-    	Points += 1
+    	Points += 5
 	break;
 	case "U2":
-    	Points += 1.5
+    	Points += 9
 	break;
     case "F":
-    	Points += 1.6
+    	Points += 17
 	break;
     case "F'":
-    	Points += 1.4
+    	Points += 23
 	break;
     case "F2":
-    	Points += 1.7
+    	Points += 19
+	break;
+    case "D":
+    	Points += 5
+	break;
+    case "D'":
+    	Points += 7
+	break;
+    case "D2":
+    	Points += 13
 	break;
 	}
 }
@@ -108,37 +123,49 @@ else if (Rthumb == 3 && Lthumb == 0) {
 	switch(turn) {
     case "R":
     	Rthumb = 0
-        Points += 1
+        Points += 5
 	break;
     case "R'":
-    	Rthumb = 0
-        Points += 1.2
+    	Rthumb = 2
+        Points += 9
 	break;
 	case "R2":
-    	Rthumb = 3
-		Points += 1.2
+    	Rthumb = 1
+		Points += 8
 	break;
     case "U":
-    	Points += 1.2
+    	Points += 8
 	break;
     case "U'":
-    	Points += 1
+    	Points += 5
 	break;
 	case "U2":
-    	Points += 1.4
+    	Points += 9
 	break;
     case "F":
-    	Points += 1
+    	Points += 5
 	break;
     case "F'":
-    	Points += 1.1
+    	Points += 7
 	break;
     case "F2":
-    	Points += 1.2
+    	Points += 8
+	break;
+    case "D":
+    	Points += 5
+	break;
+    case "D'":
+    	Points += 7
+	break;
+    case "D2":
+    	Points += 13
 	break;
 	}
 }
 else if (Rthumb == 2) {
-Points += 3
+Rthumb = 0
+Points += 5
+move(turn)
 }
 }
+</script>
